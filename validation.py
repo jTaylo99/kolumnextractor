b46a372d18680f2ec7f2f87495072b365bae570d
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2022-10-14 11:00:58 +0800

    ♻️ Use f-string

--- validation.py
+++ validation.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class Validator(ABC):
     def __set_name__(self, owner, name):
-        self.private_name = '_' + name
+        self.private_name = f'_{name}'
 
     def __get__(self, obj, objtype=None):
         return getattr(obj, self.private_name)
