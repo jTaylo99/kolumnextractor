785d1a3d0b46f5bba49b9d5ecf620b7e3011067b
Geoff Leyland <geoff.leyland@fonterra.com>
2022-10-28 23:05:52 +1300

    Fix tests.
    Fix the tests so they work (they don't do as much yet), they use pytest
    (which I've added to requirements.txt - I don't know how to do test
    requirements - do things like that exist in python?) and added a
    .travis.yml.  I suspect that Jack will get himself a travis account for
    that to work.

--- src/test_kolumnextractor.py
+++ src/test_kolumnextractor.py
@@ -1,39 +1,21 @@
 import unittest
 
-from kolumnextractor import reading
+import kolumnextractor as kea
+
+test_file = "data/test.csv"
+
 
 class TestMain(unittest.TestCase):
-	def test01(self):
-		"Check if it can extract column 1"
-		test_file = "../data/test.csv"
-		test_column = "column 1"
-		expected_result = [['column 1'], ['1']]
-		test = reading(filepath=test_file, columns=[test_column])
-		self.assertEqual(test, expected_result)
+    def test01(self):
+        "Check if it can extract column 1"
+        test_column = "column 1"
+        class Name(kea.Data):
+            _columns = {
+                "column1": kea.Number(minvalue=1, maxvalue=100)
+            }
+        expected_result = [['column 1'], ['1']]
+        test = kea.reading_data(filepath=test_file, columns=Name)
 
-	def test02(self):
-		"Check if it can extract column 1 and column 2"
-		test_file = "../data/test.csv"
-		test_column = ["column 1", "column 2"]
-		expected_result = [['column 1', 'column 2'], ['1', '2']]
-		test = reading(filepath=test_file, columns=test_column)
-		self.assertEqual(test, expected_result)
-
-	def test03(self):
-		"Check if it can extract column 1, column 2, and column 3"
-		test_file = "../data/test.csv"
-		test_column = ["column 1", "column 2", "column 3"]
-		expected_result = [['column 1', 'column 2', 'column 3'], ['1', '2', '3']]
-		test = reading(filepath=test_file, columns=test_column)
-		self.assertEqual(test, expected_result)
-
-	def test04(self):
-		"Check if it can extract column 2, and column 3"
-		test_file = "../data/test.csv"
-		test_column = ["column 2", "column 3"]
-		expected_result = [['column 2', 'column 3'], ['2', '3']]
-		test = reading(filepath=test_file, columns=test_column)
-		self.assertEqual(test, expected_result)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
