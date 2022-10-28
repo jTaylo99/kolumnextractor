db637f80f13bfb5dfeeb1361b261aeacfd9ab830
bissella <alex.bissell@fonterra.com>
2022-10-28 15:21:54 +1300

    dictionary for unit converter

--- transformers.py
+++ transformers.py
@@ -34,4 +34,39 @@ def normalise(string):
         this is-a string @swell & so is th1s => thisisastringswellsoisths
     """
     return sub(r"[\W_]+", "", string)
-        
+
+def unit_converter(value, numerator_conversion, denominator_conversion)
+    """unit_converter
+    Convert a number into a specified unit type from a specified unit type.
+    
+    Args:
+        value: (int, float) to be converted
+        Unit_from: string of the units the data is currently in.
+        Units_to: string of the desired units.
+    
+    Returns:
+        Number that has had unit conversion. For example:
+        10 [m3/s] = 10000/60 [l/min]
+        """
+
+#Creating conversion dictionary
+conversion_dict = {("l","m3"): 1000,
+            ("m3", "l"): 0.001,
+            ("s", "min"): 1/60,
+            ("min", "s"): 60,
+            ("s", "hr"): 3600,
+            ("hr", "s"): 1/3600,
+            ("min", "hr"): 1/60,
+            ("hr", "min"): 60,
+            ("1","1"): 1}
+
+# Need a way to look up the dictionary based on the function calls.
+# How do you look up dictionary values.
+
+
+
+    """
+    1) Create a list with basic volume units [volume(L,m3)] Mass, time. 
+    2) Create an logic flow (if diagram) to convert the units.
+    3) Pass the function. 
+    """
