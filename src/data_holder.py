eb92fa4ee76b212735544b83ba88c7f964feb81a
kimessenbergfonterra <101370789+kimessenbergfonterra@users.noreply.github.com>
2022-10-31 11:09:34 +1300

    another merge conflict

--- src/data_holder.py
+++ src/data_holder.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:data_holder.py
 from validation import Number
 
 import logging
@@ -53,59 +52,3 @@ if __name__ == '__main__':
             "Column 3": Number(minvalue=1, maxvalue=100),
         }
     name_row = Name(**{"Column 1":1, "Column 2":1, "Column 3":1})
-=======
-from validation import Number
-
-import logging
-logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-
-class Data:
-    _columns = {}
-
-    def __init__(self, **kwargs):
-        if len(kwargs) != len(self._columns):
-            msg = f'Expected {len(self._columns)} arguments.'
-            logging.error(msg)
-            raise TypeError(msg)
-
-        for (column, validator) in self._columns.items():
-            if validator is None:
-                setattr(self, column, kwargs[column])
-            else:
-                validator.set_name(column)
-                validator.__set__(self, kwargs[column])
-
-    @property
-    def columns(self):
-        return self._columns
-
-
-class DataContainer:
-    _type_of_row = Data
-    data = []
-
-    def __init__(self, data: list[Data]):
-        for row in data:
-            if not isinstance(row, self._type_of_row):
-                raise TypeError()
-            self.data.append(row)
-
-    @property
-    def _columns(self):
-        return self._type_of_row.columns
-
-    def validate_row(self, row: Data):
-        if list(row.__dict__.keys()) != self._columns:
-            raise TypeError
-
-
-if __name__ == '__main__':
-    class Name(Data):
-        _columns = {
-            "Column 1": Number(minvalue=1, maxvalue=100),
-            "Column 2": Number(minvalue=1, maxvalue=100),
-            "Column 3": Number(minvalue=1, maxvalue=100),
-        }
-    name_row = Name(**{"Column 1":1, "Column 2":1, "Column 3":1})
->>>>>>> 93500df3721edd5ffe7ee58865cf94c7beb8afe2:src/data_holder.py
